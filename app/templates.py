7c0a12456272ced9bee65424c6ae086776fd183e
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-11-05 16:39:01 +0100

    Fixing utf8

--- app/templates.py
+++ app/templates.py
@@ -6,7 +6,7 @@ from mako.runtime import Context
 from mako.lookup import TemplateLookup
 
 def RenderTemplate(file, **kwargs):
-	templates = TemplateLookup(directories=["./templates/"], module_directory="./modules/")
+	templates = TemplateLookup(directories=["./templates/"], module_directory="./modules/", default_filters=['decode.utf8'], input_encoding='utf-8', output_encoding='utf-8')
 	template = templates.get_template(file)
 	
 	if 'Benutzername' in cherrypy.session:
