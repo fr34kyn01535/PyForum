131f1556050bbb37c591d5e1ae049a2e4a82157d
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-12-05 19:51:27 +0100

    Style fixes

--- modules/diskussionen-liste.html.py
+++ modules/diskussionen-liste.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1449338072.3068676
+_modified_time = 1449341405.444031
 _enable_loop = True
 _template_filename = 'templates/diskussionen-liste.html'
 _template_uri = 'diskussionen-liste.html'
@@ -17,9 +17,9 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        diskussionen = context.get('diskussionen', UNDEFINED)
-        thema = context.get('thema', UNDEFINED)
         role = context.get('role', UNDEFINED)
+        thema = context.get('thema', UNDEFINED)
+        diskussionen = context.get('diskussionen', UNDEFINED)
         __M_writer = context.writer()
         for diskussion in diskussionen:
             __M_writer('\r\n\t<div class="list-group-item">\r\n\t\t<div class="row-action-primary">\r\n\t\t\t<i class="mdi-file-folder"></i>\r\n\t\t</div>\r\n\t\t<div class="row-content">\r\n\t\t\t<div class="least-content"></div>\r\n\r\n')
@@ -40,7 +40,7 @@ def render_body(context,**pageargs):
                 __M_writer('\t\t\t\t<button style="float:right;" value="')
                 __M_writer(filters.decode.utf8(diskussion["ID"]))
                 __M_writer('" class="deleteDiscussion btn btn-warning btn-xs"><i class="mdi-content-clear"></i></button>\r\n')
-            __M_writer('\t\t\t\t\t\t\t\r\n\t\t\t<br/>\r\n\r\n\t\t\t<p class="list-group-item-text">von ')
+            __M_writer('\t\t\t\t\t\r\n\r\n\t\t\t<p class="list-group-item-text">von ')
             __M_writer(filters.decode.utf8(diskussion["Ersteller"]))
             __M_writer(' (')
             __M_writer(filters.decode.utf8(diskussion["Erstellt"]))
@@ -59,6 +59,6 @@ def render_body(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"uri": "diskussionen-liste.html", "line_map": {"16": 0, "24": 1, "25": 2, "26": 10, "27": 11, "28": 11, "29": 11, "30": 12, "31": 13, "32": 13, "33": 13, "34": 13, "35": 13, "36": 13, "37": 13, "38": 15, "39": 17, "40": 18, "41": 18, "42": 18, "43": 20, "44": 23, "45": 23, "46": 23, "47": 23, "48": 25, "49": 26, "50": 26, "51": 26, "52": 26, "53": 26, "54": 28, "60": 54}, "source_encoding": "utf-8", "filename": "templates/diskussionen-liste.html"}
+{"uri": "diskussionen-liste.html", "line_map": {"16": 0, "24": 1, "25": 2, "26": 10, "27": 11, "28": 11, "29": 11, "30": 12, "31": 13, "32": 13, "33": 13, "34": 13, "35": 13, "36": 13, "37": 13, "38": 15, "39": 17, "40": 18, "41": 18, "42": 18, "43": 20, "44": 22, "45": 22, "46": 22, "47": 22, "48": 24, "49": 25, "50": 25, "51": 25, "52": 25, "53": 25, "54": 27, "60": 54}, "filename": "templates/diskussionen-liste.html", "source_encoding": "utf-8"}
 __M_END_METADATA
 """
--- modules/header.html.py
+++ modules/header.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1447151039.4356701
+_modified_time = 1449340931.2884288
 _enable_loop = True
 _template_filename = 'templates/header.html'
 _template_uri = 'header.html'
@@ -16,14 +16,14 @@ _exports = []
 def render_body(context,title,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        __M_locals = __M_dict_builtin(pageargs=pageargs,title=title)
-        role = context.get('role', UNDEFINED)
+        __M_locals = __M_dict_builtin(title=title,pageargs=pageargs)
         username = context.get('username', UNDEFINED)
+        role = context.get('role', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('<!DOCTYPE html>')
-        __M_writer('\r\n<html>\r\n   <head>\r\n      <title>PyForum | ')
+        __M_writer('\r\n<html>\r\n   <head>\r\n\t\t<title>PyForum | ')
         __M_writer(filters.decode.utf8(title))
-        __M_writer('</title>\r\n      <meta charset="UTF-8" />    \r\n        <link href="/css/bootstrap.min.css" rel="stylesheet">\r\n        <link href="/css/roboto.min.css" rel="stylesheet">\r\n        <link href="/css/material-fullpalette.min.css" rel="stylesheet">\r\n        <link href="/css/ripples.min.css" rel="stylesheet">\r\n\t\t\r\n\t\t<script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/bootstrap.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/material.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/ripples.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/custom.js"></script>\r\n\t\t\r\n\t\t<link href="/css/custom.css" rel="stylesheet" />\r\n\t\t\r\n\t\t<script type="text/javascript">\r\n\t\t\t$(function(){\r\n\t\t\t\t$.material.init();\r\n\t\t\t});\r\n\t\t</script>\r\n\t\t\r\n   </head>\r\n   <body>\r\n      <nav id="navigation">\r\n\t  \r\n\t  <div class="navbar navbar-default">\r\n  <div class="navbar-header">\r\n    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">\r\n      <span class="icon-bar"></span>\r\n      <span class="icon-bar"></span>\r\n      <span class="icon-bar"></span>\r\n    </button>\r\n    <a class="navbar-brand" href="/">PyForum</a>\r\n  </div>\r\n  <div class="navbar-collapse collapse navbar-responsive-collapse">\r\n    <ul class="nav navbar-nav">\r\n\t\t<li><a href="/">Startseite</a></li>\r\n')
+        __M_writer('</title>\r\n\t\t<meta charset="UTF-8" />    \r\n\t\t<link rel="shortcut icon" type="image/x-icon" href="/css/favicon.ico">\r\n\t\t<link href="/css/bootstrap.min.css" rel="stylesheet">\r\n\t\t<link href="/css/roboto.min.css" rel="stylesheet">\r\n\t\t<link href="/css/material-fullpalette.min.css" rel="stylesheet">\r\n\t\t<link href="/css/ripples.min.css" rel="stylesheet">\r\n\r\n\t\t<script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/bootstrap.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/material.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/ripples.min.js"></script>\r\n\t\t<script type="text/javascript" src="/js/custom.js"></script>\r\n\r\n\t\t<link href="/css/custom.css" rel="stylesheet" />\r\n\r\n\t\t<script type="text/javascript">\r\n\t\t\t$(function(){\r\n\t\t\t\t$.material.init();\r\n\t\t\t});\r\n\t\t</script>\r\n\t\t\r\n   </head>\r\n   <body>\r\n      <nav id="navigation">\r\n\t  \r\n\t  <div class="navbar navbar-default">\r\n  <div class="navbar-header">\r\n    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">\r\n      <span class="icon-bar"></span>\r\n      <span class="icon-bar"></span>\r\n      <span class="icon-bar"></span>\r\n    </button>\r\n    <a class="navbar-brand" href="/">PyForum</a>\r\n  </div>\r\n  <div class="navbar-collapse collapse navbar-responsive-collapse">\r\n    <ul class="nav navbar-nav">\r\n\t\t<li><a href="/">Startseite</a></li>\r\n')
         if role=="Administrator": 
             __M_writer('\t\t\t<li><a href="/administration">Administration</a></li>\r\n')
         __M_writer('    </ul>\r\n\t<ul class="nav navbar-nav navbar-right">\r\n           \r\n')
@@ -35,7 +35,7 @@ def render_body(context,title,**pageargs):
             __M_writer('"><a href="#">')
             __M_writer(filters.decode.utf8(username))
             __M_writer('</a></li><li><a style="padding:16px !important;" href="/logout" title="Ausloggen"><i class="mdi-action-lock-outline"></i></a></li>\r\n')
-        __M_writer('\t\t\t\r\n\t\t\t\r\n\t\t\t\r\n\t\t\r\n\t\t\r\n        </ul>\r\n  </div>\r\n</div>\r\n      </nav>\r\n\t\r\n\t  \r\n      <section id="main">')
+        __M_writer('\t\t\t\r\n\t\t\r\n        </ul>\r\n  </div>\r\n</div>\r\n      </nav>\r\n\t\r\n\t  \r\n      <section id="main">')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -43,6 +43,6 @@ def render_body(context,title,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"source_encoding": "utf-8", "line_map": {"32": 50, "33": 51, "34": 51, "35": 51, "36": 51, "37": 51, "38": 53, "44": 38, "16": 2, "23": 2, "24": 2, "25": 5, "26": 5, "27": 42, "28": 43, "29": 45, "30": 48, "31": 49}, "uri": "header.html", "filename": "templates/header.html"}
+{"uri": "header.html", "line_map": {"32": 51, "33": 52, "34": 52, "35": 52, "36": 52, "37": 52, "38": 54, "44": 38, "16": 2, "23": 2, "24": 2, "25": 5, "26": 5, "27": 43, "28": 44, "29": 46, "30": 49, "31": 50}, "filename": "templates/header.html", "source_encoding": "utf-8"}
 __M_END_METADATA
 """
